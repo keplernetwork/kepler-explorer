369453d4c08c12a65c813a7ba95e15e3352d5458
keplernetwork <keplernetwork@users.noreply.github.com>
2019-03-08 10:10:36 +0100

    Update URL

--- keplerexplorer/keplerexplorer/settings.py
+++ keplerexplorer/keplerexplorer/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "keplerexplorer.net"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "explorer.kepler.network"]
 
 
 # Application definition
